41c9b9be33a9bcb6368a55c280f7059eb5c3ea9f
clairesheek <clairesheek@gmail.com>
2021-05-27 11:51:38 -0500

    tried adding server prefix to start script

--- server/app.js
+++ server/app.js
@@ -15,7 +15,7 @@ const port = process.env.PORT || 3000
 
 //database connection Option 1
 
-const dbURI = "mongodb+srv://MVPUser:MVPshop123@mvp.sqwsb.mongodb.net/sample_restaurants?retryWrites=true&w=majority"
+const dbURI = "mongodb+srv://MVPUser:MVPshop123@mvp.sqwsb.mongodb.net/test_db?retryWrites=true&w=majority"
 mongoose.connect(dbURI, {useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true }).then((result) => app.listen(port)).catch((err) => console.log(err))
 
 //database connection Option 2
@@ -35,29 +35,29 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/", indexRouter);
-app.use("/users", usersRouter);
-
-// catch 404 and forward to error handler
-app.use(function (req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render("error");
-});
+// app.use("/", indexRouter);
+// app.use("/users", usersRouter);
 
 app.get('/', (req, res) => {
   res.send('Welcome to our server')
 })
+
+// // catch 404 and forward to error handler
+// app.use(function (req, res, next) {
+//   next(createError(404));
+// });
+
+// // error handler
+// app.use(function (err, req, res, next) {
+//   // set locals, only providing error in development
+//   res.locals.message = err.message;
+//   res.locals.error = req.app.get("env") === "development" ? err : {};
+
+//   // render the error page
+//   res.status(err.status || 500);
+//   res.render("error");
+// });
+
 
 module.exports = app;
