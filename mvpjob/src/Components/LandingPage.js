63dcb739ee96d981a56712af1d41b9f54021948b
Connor Yates <connoryates@Connors-MacBook-Pro.local>
2021-05-28 13:26:10 -0500

    login page and navbar without functionality

---
+++ mvpjob/src/Components/LandingPage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Navigation from './Navigation';
+
+
+const LandingPage=()=>{
+    return(
+        
+ <div>
+    <Navigation />
+ </div>
+        
+    )
+}
+
+export default LandingPage;
