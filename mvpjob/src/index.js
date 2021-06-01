39727098b2d3c8ff1a81a1e75cca372cfd5a9136
MariaGretaSepulveda <sepulveda.greta@gmail.com>
2021-06-01 13:36:34 -0500

    removed package lock jason

--- mvpjob/src/index.js
+++ mvpjob/src/index.js
@@ -1,13 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-<<<<<<< HEAD
-import reportWebVitals from './reportWebVitals';
-
-
-=======
 import 'bootstrap/dist/css/bootstrap.min.css';
->>>>>>> 250cc2c32ffc1af54934859f74041cd63d70a3ab
 ReactDOM.render(
   <React.StrictMode>
     <App />
