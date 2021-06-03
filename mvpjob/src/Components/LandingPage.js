0d924f7a030952f736ca949e9042818b3f5a25b9
MariaGretaSepulveda <sepulveda.greta@gmail.com>
2021-06-02 23:11:10 -0500

    updated landing page

--- mvpjob/src/Components/LandingPage.js
+++ mvpjob/src/Components/LandingPage.js
@@ -1,16 +1,62 @@
 import React from 'react';
 import Navigation from './Navigation';
 
-
-const LandingPage=()=>{
-    return(
-        
-        <div>
-        <Navigation />
-       <h2>The Landing Page</h2> 
- </div>
-        
+export const Navigation = (props) => {
+    return (
+      <nav id='menu' className='navbar navbar-default navbar-fixed-top'>
+        <div className='container'>
+          <div className='navbar-header'>
+            <button
+              type='button'
+              className='navbar-toggle collapsed'
+              data-toggle='collapse'
+              data-target='#bs-example-navbar-collapse-1'
+            >
+              {' '}
+              <span className='sr-only'>Toggle navigation</span>{' '}
+              <span className='icon-bar'></span>{' '}
+              <span className='icon-bar'></span>{' '}
+              <span className='icon-bar'></span>{' '}
+            </button>
+            <a className='navbar-brand page-scroll' href='#page-top'>
+              My Landing Page
+            </a>{' '}
+          </div>
+  
+          <div
+            className='collapse navbar-collapse'
+            id='bs-example-navbar-collapse-1'
+          >
+            <ul className='nav navbar-nav navbar-right'>
+              <li>
+                <a href='#features' className='page-scroll'>
+                  Features
+                </a>
+              </li>
+              <li>
+                <a href='#about' className='page-scroll'>
+                  About
+                </a>
+              </li>
+              <li>
+                <a href='#Where' className='page-scroll'>
+                  Locatioon
+                </a>
+              </li>
+              <li>
+                <a href='#JobSearch' className='page-scroll'>
+                  jobs available
+                </a>
+              </li>
+              <li>
+                <a href='#About us' className='page-scroll'>
+                  Avatars
+                </a>
+              </li>
+              
+            </ul>
+          </div>
+        </div>
+      </nav>
     )
-}
-
-export default LandingPage;
+  }
