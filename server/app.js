97c47928e19645493ccd28f8b217b701b5a29a74
Austin Skrobarczyk <austin.skrobar@gmail.com>
2021-06-04 11:40:07 -0500

    updates to resume and comment on usermodel to add timestamp

--- server/app.js
+++ server/app.js
@@ -7,7 +7,7 @@ const experienceRouter = require("./routes/experience");
 const usersRouter = require("./routes/users");
 
 const app = express();
-// const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 
 //establishing database connection
 const dbURI =
