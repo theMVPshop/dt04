74813c992faf96b4f1732b0b2ebe4d41a2ddc55b
clairesheek <clairesheek@gmail.com>
2021-06-10 11:25:46 -0500

    working on API Data manipulation

--- server/controller/searchController.js
+++ server/controller/searchController.js
@@ -1,12 +1,10 @@
 const fetch = require("node-fetch");
 
+
 const handleError = (res, err) => {
   console.error('Error: ', {err})
   return res.status(500).send('An unexpected error has occured.', err)
 }
-
-//Indeed Postings:
-var indeedData = {}
 
 //USA JOBS CONNECTION INFO
 var host = 'data.usajobs.gov';  
@@ -16,17 +14,17 @@ var authKey = '+B7adBgiL5PUqf8xnx64GZjI1Xldz+HS0s/xZrYqrCw=';
 
 //Controller function to call both APIs
 const fetchResults = async (req, res) => {
-  // const position = "medical+assistant" //req.body.position
-  const positionUSA =  "medical%20assistant"
-  // const location = "austin+texas" //req.body.location
-  const locationUSA = "austin%20tx" //req.body.location
+
+	var jobList = []
+  const position =  "medical%20assistant" //req.body.location
+  const location = "austin%20tx" //req.body.location
 
 
   // const adzunaURL = `https://api.adzuna.com/v1/api/jobs/us/search/1?app_id=d8054f18&app_key=91e9f06279e878970e11dee09fd5f833&results_per_page=20&what=${position}&where=${location}&content-type=application/json`
-  const usaJobsURL = `https://data.usajobs.gov/api/Search?PositionTitle=${positionUSA}&LocationName=${locationUSA}`
+  const usaJobsURL = `https://data.usajobs.gov/api/Search?PositionTitle=${position}&LocationName=${location}`
 
-  Promise.all([
-	fetch(adzunaURL),
+  // Promise.all([
+	// fetch(adzunaURL),
 	fetch(usaJobsURL, {            
     method: 'GET',      
     headers: {          
@@ -34,70 +32,27 @@ const fetchResults = async (req, res) => {
         "User-Agent": userAgent,          
         "Authorization-Key": authKey      
     }})
-  ])
-  // .then(response => response.json())
-  .then(function (responses) {
+  // ])
+  .then(response => response.json())
+  // .then(function (responses) {
     // Get a JSON object from each of the responses
-    return Promise.all(responses.map(function (response) {
-      return response.json();
-    }))
-  })
-  .then(data => res.send(data))
+    // return Promise.all(responses.map(function (response) {
+    //   return response.json();
+    // }))
+  // })
+  .then(data =>  res.send(data.SearchResult.SearchResultItems))
+  
+	// .then(data =>  {
+	// 	res.send(Object.keys(data.SearchResult.SearchResultItems))
+	// })
 
+	// .then(data =>  res.send(data.SearchResult.SearchResultItems).map((job, idx) => (
+	// 	console.log(job.MatchedObjectDescriptor)
+	// 	))
 
+	.then(data => res(data))
+	// )
 };
-
-var JQUERYconvertRSS;
-
-(function ($) {
-	JQUERYconvertRSS = function (opt) {
-		var def = $.extend(
-			{
-				MaxCount: 25,
-				ShowDesc: true,
-				ShowPubDate: true,
-				DescCharacterLimit: 0,
-				TitleLinkTarget: "_blank",
-				DateFormat: "",
-				DateFormatLang: "en",
-			},
-			opt
-		);
-
-		var id = $(this).attr("id"),
-			s = "",
-			dt;
-		$("#" + id).empty();
-		if (def.FeedUrl == undefined) return;
-		$.ajax({
-			url:
-				"https://feed.jquery-plugins.net/load?url=" +
-				encodeURIComponent(def.FeedUrl) +
-				"&maxCount=" +
-				def.MaxCount +
-				"&dateCulture=" +
-				def.DateFormatLang +
-				"&dateFormat=" +
-				def.DateFormat,
-			dataType: "json",
-			success: function (result) {
-				$("#" + id).empty();
-				if (result.data == null) return;
-//////////////// data is returned
-         console.log(result.data);
-        
-			},
-		});
-	};
-})(jQuery);
-
-const covertToJSON = () => {
-  JQUERYconvertRSS({
-        FeedUrl:"https://rss.indeed.com/rss?q=medical+assistant&l=Austin,+TX&jt=parttime",
-        MaxCount: 120,
-      })
-}
-
 
 module.exports = { fetchResults };
 
