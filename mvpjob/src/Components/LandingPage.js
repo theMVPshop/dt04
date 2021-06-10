6211f85feab4cd604932cf16a4f6bc4b1f2e6a80
Austin Skrobarczyk <austin.skrobar@gmail.com>
2021-06-10 12:47:48 -0500

    removed nav tag in landing and navigation import on login

--- mvpjob/src/Components/LandingPage.js
+++ mvpjob/src/Components/LandingPage.js
@@ -1,10 +1,55 @@
 import React from "react";
 
-
 const LandingPage = () => {
   return (
-    <div>
-    </div>
+      <div className="container">
+        <div className="navbar-header">
+          <button
+            type="button"
+            className="navbar-toggle collapsed"
+            data-toggle="collapse"
+            data-target="#bs-example-navbar-collapse-1"
+          >
+            {" "}
+            <span className="sr-only">Toggle navigation</span>{" "}
+            <span className="icon-bar"></span>{" "}
+            <span className="icon-bar"></span>{" "}
+            <span className="icon-bar"></span>{" "}
+          </button>
+          <a className="navbar-brand page-scroll" href="#page-top">
+            Medical Externiship App
+          </a>{" "}
+        </div>
+
+        <div
+          className="collapse navbar-collapse"
+          id="bs-example-navbar-collapse-1"
+        >
+          <ul className="nav navbar-nav navbar-right">
+            <li>
+              <a href="#Jobtype" className="page-scroll">
+                Type of Jobs
+              </a>
+            </li>
+            <li></li>
+            <li>
+              <a href="#JobLocation" className="page-scroll">
+                Locations
+              </a>
+            </li>
+            <li>
+              <a href="#JobSearch" className="page-scroll">
+                Jobs available
+              </a>
+            </li>
+            <li>
+              <a href="#About us" className="page-scroll">
+                The team <img></img>
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
   );
 };
 
