5260ac68225b89862dd9b99fc082e26166d2c86e
Austin Skrobarczyk <austin.skrobar@gmail.com>
2021-06-11 11:43:03 -0500

    working on adding cookies and tieing them in with the resume

--- mvpjob/src/Components/About.js
+++ mvpjob/src/Components/About.js
@@ -1,25 +1,11 @@
 import React from "react";
-import "./About.css";
- 
-const About=()=>{
-    return(
-        <div className="about">
-            <Navigation/>
-            <h2>Medical Job Search</h2>
-        </div>
-    )}
-        <P>Welcome to Medical Job Search , our number one focus is helping you find your dream job in the medical field.
 
-
-        Founded in year by 2021, Medical Job Search  has come a long way from its beginnings in Austin, Texas. When all of us first started out, our passion for Medical Field app - e.g. Medical Job Search drove  us to action: quit day job, do tons of research, etc. so that Medical Job Search can offer you a competitive job search in exactly what you are looking for. We now serve customers all over town, country, the world, and are thrilled that we're able to turn our passion into our own website.
-        
-        
-        We hope you enjoy our products as much as we enjoy offering them to you. If you have any questions or comments, please don't hesitate to contact us.
-         </P>
-        
-    
-    
-
-
+const About = () => {
+  return (
+    <div className="about">
+      <h2>This is the about page</h2>
+    </div>
+  );
+};
 
 export default About;
