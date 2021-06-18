0088971d92e87ca767e5609dfd3f9047722be3e5
MariaGretaSepulveda <sepulveda.greta@gmail.com>
2021-06-18 13:23:05 -0500

    updat4d about page and images

--- mvpjob/src/App.js
+++ mvpjob/src/App.js
@@ -170,7 +170,7 @@ const App = () => {
           <Route exact path="/SignUp" component={SignUp}></Route>
           <Route exact path="/resume" component={Resume}></Route>
           <Route exact path="/popup" component={PopUp}></Route>
-
+         
         </Switch>
       </Router>
     </Fragment>
