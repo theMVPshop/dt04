62dfb2958836d1049b12bcff370ae4bc9dac7126
clairesheek <clairesheek@gmail.com>
2021-06-25 12:16:10 -0500

    working on heroku deploy

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
-const createError = require("http-errors");
+// const createError = require("http-errors");
 const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const mongoose = require("mongoose");
+// const cors = require("cors")
+
 const experienceRouter = require("./routes/experience");
 const usersRouter = require("./routes/users");
 const searchRouter = require("./routes/search");
-const cors = require("cors")
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -27,25 +28,25 @@ mongoose
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(cors({ origin: 'http://localhost:3000' , credentials :  true}));
-
-
-app.use("/api/users", usersRouter);
-app.use("/api/experience", experienceRouter);
-app.use("/api/search", searchRouter)
+// app.use(cors({ origin: 'http://localhost:3000' , credentials :  true}));
 
 // Serve static files from the React frontend app
 app.use(express.static(path.join(__dirname, "/client/build")));
 
-
-app.get("api/", (req, res) => {
-  res.send("Welcome to our server");
-});
+app.use("/api/users", usersRouter);
+app.use("/api/experience", experienceRouter);
+app.use("/api/search", searchRouter)
 
 
 // AFTER defining routes: Anything that doesn't match what's above, send back index.html; (the beginning slash ('/') in the string is important!)
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname + "/client/build/index.html"));
 });
+
+app.get("api/", (req, res) => {
+  res.send("Welcome to our server");
+});
+
+
 
 module.exports = app;
