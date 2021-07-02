05661b05da41fdf2b66cbe9a4511e93d17fc9331
AustinSkrobar <austin.skrobar@gmail.com>
2021-07-02 12:36:58 -0500

    uncomment exp get again

--- client/src/Components/ResumeView.js
+++ client/src/Components/ResumeView.js
@@ -2,17 +2,19 @@ import React, { useState, useEffect } from "react";
 import axios from 'axios'
 
 const ResumeView = (props) => {
-    const [resume, setResume] = useState({ });
-      useEffect(() => {
-          console.log('helloWorld')
-          axios.get(`/api/expierence/${props.user}`).then(res => console.log("res view: ", res)).catch(err => console.log(err))
-      }, [props.user])
+  const [resume, setResume] = useState({});
+  useEffect(() => {
+    console.log('helloWorld')
+    axios.get(`/api/expierence/${props.user}`).then(res => {console.log("res view: ", res}, (error) => {
+      console.log('err :', error)
+    })
+  }, [props.user])
 
-      return (
-        <div >
-          <h3>Resusme Builder{ console.log(resume), console.log("props :", props.user)}</h3>
-        </div>
-      )
+  return (
+    <div >
+      <h3>Resusme Builder{console.log(resume), console.log("props :", props.user)}</h3>
+    </div>
+  )
 }
 
 export default ResumeView
