84e29bd647139d71f9376446147dddbf7c3348bc
MariaGretaSepulveda <sepulveda.greta@gmail.com>
2021-07-02 13:37:16 -0500

    updated css and about js content and removed images not needed

--- client/src/Components/About.js
+++ client/src/Components/About.js
@@ -16,15 +16,7 @@ const About = () => {
       <div className="Logo">
          <img src={Logo} alt="Our Logo"/> 
       </div>
-       <div className ="Gallery">
-         <img src={Claire} alt="Claire"/>
-         <img src={Austin} alt="Austin"/>
-         <img src={Keith}  alt="Keith"/>
-         <img src={Connor} alt="Connor"/>
-         <img src={Greta} alt ="Greta"/>
-       </div>
-
-<h2>Welcome to the Medical Assistant Program, your number one source for searching a job in your field. We're dedicated to giving you the very best when looking for your new role in the Medical Assistance field, with a focus on your dream Medical Assistance job.
+      <h2>Welcome to the Medical Assistant Program, your number one source for searching a job in your field. We're dedicated to giving you the very best when looking for your new role in the Medical Assistance field, with a focus on your dream Medical Assistance job.
 
 
 Founded in 2021 by Claire Sheek, Austin Skrobarczyk, Connor Yates, Keith Kritselis, and Greta Sepulveda.  Medical Assistant Job Search has come a long way from it's beginnings in Austin, Texas. When we the founders first started out, our passion for Medical Job Assistance it drove us to action we quit our day jobs, did tons of research, so that Medical Assistance Job Search can offer you competitive Medical Assistance position. We now serve clients all over Central Texas and are thrilled that we're able to turn our passion into our own website.
@@ -37,6 +29,17 @@ We hope you enjoy our Medical Assistance job search website as much as we enjoy 
 All the best,
 
   Medical Assistance team</h2>
+
+       <div className ="Gallery">
+         <img src={Claire} alt="Claire"/>
+         <img src={Austin} alt="Austin"/>
+         <img src={Keith}  alt="Keith"/>
+         <img src={Connor} alt="Connor"/>
+         <img src={Greta} alt ="Greta"/>
+       </div>
+
+
+
     </div>
 
 )};
