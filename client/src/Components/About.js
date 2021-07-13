f0cf89c632a9a1c9102a46e458be8d42495c200a
MariaGretaSepulveda <sepulveda.greta@gmail.com>
2021-07-13 12:51:00 -0500

    updated p info

--- client/src/Components/About.js
+++ client/src/Components/About.js
@@ -18,12 +18,8 @@ const About = () => {
     <div className="about">
 
 
-      
-        
-
-
-      <body>
-        <div class="container">
+<body>
+<div class="container">
 <p>Welcome to the Medical Assistant Program, your number one source for searching a job in your field. We're dedicated to giving you the very best when looking for your new role in the Medical Assistance field, with a focus on your dream Medical Assistance job.
 Founded in 2021 by Claire Sheek, Austin Skrobarczyk, Connor Yates, Keith Kritselis, Dwaylan Applewhite and Greta Sepulveda.  Medical Assistant Job Search has come a long way from it's beginnings in Austin, Texas. When we the founders first started out, our passion for Medical Job Assistance it drove us to action we quit our day jobs, did tons of research, so that Medical Assistance Job Search can offer you competitive Medical Assistance position. We now serve clients all over Central Texas and are thrilled that we're able to turn our passion into our own website.
 We hope you enjoy our Medical Assistance job search website as much as we enjoy offering it to you. If you have any questions or comments, please don't hesitate to contact us.
@@ -49,3 +45,4 @@ Medical Assistance team</p>
 };
 
 export default About;
+
